946e299b8069c63ede4ffadf490ee5780cfe609f
GeorgessDavid <georgessdavid@gmail.com>
2022-09-09 17:34:40 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let express = require('express');
+
+let app = express();
+
+let path = require('path');
+
+const publicPath = path.join(__dirname, '/public');
+
+
+app.listen(3000, () => {
+    console.log("Servidor activo en el puerto 3000.")
+})
+
+app.use(express.static((__dirname + '/public')));
+
+app.get('/', (req,res) => {
+
+    res.sendFile((__dirname + '/views/home.html'));
+
+})
+
